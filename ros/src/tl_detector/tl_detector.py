30301ea1f27ae53d71b6e8f7f5260b018369ad31
Clara-YR <30821609+Clara-YR@users.noreply.github.com>
2018-10-03 11:00:54 +0800

    ensure closest ahead, tl_detector
    ensure the closest waypoint is ahead of the vehicle

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -137,6 +137,17 @@ class TLDetector(object):
         """
         #TODO implement
         closest_idx = self.waypoint_tree.query([x, y], 1)[1]
+        # check is the closest wp is ahead of the vehicle
+        prev_coord = self.waypoints_2d[closest_idx-1]
+        
+        # equation for hyperplane through closest_coords
+        cl_vect = np.array(closest_coord)
+        prev_vect = np.array(prev_coord)
+        pos_vect = np.array([x, y])
+
+        val = np.dot(cl_vect - prev_vect, pos_vect - cl_vect)
+        if val > 0:
+            closest_idx = (closest_idx + 1) % len(self.waypoints_2d)
         return closest_idx
 
     def get_light_state(self, light):
